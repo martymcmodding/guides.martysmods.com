2ed0eb78db952a66913c1ce2fab24be34bf10cfd
Jordan Brady <jorban@martysmods.com>
2025-05-04 16:13:34 -0500

    fix for mobile >:
    mobile makes nothing fun.

--- src/theme/NotFound/Content/index.js
+++ src/theme/NotFound/Content/index.js
@@ -6,20 +6,23 @@ export default function NotFoundContent({ className }) {
   return (
     <main className={clsx('container margin-vert--xl', className)}>
       <div className="row">
-        <div className="col col--6 col--offset-3">
+        <div className="col col--12"> { }
           <Heading as="h1" className="hero__title text--center">
             Page Not Found
           </Heading>
-          <div style={{ textAlign: 'center' }}>
+          <div style={{ textAlign: 'center', overflowX: 'auto' }}>
             <pre
               style={{
                 fontFamily: 'monospace',
-                fontSize: '1.3rem',
+                fontSize: '1rem',
                 lineHeight: '1.2',
                 display: 'inline-block',
                 backgroundColor: 'transparent',
                 border: 'none',
-                color: 'green'
+                color: 'green',
+                whiteSpace: 'pre',
+                padding: '1rem',
+                minWidth: 'fit-content',
               }}
             >
               {String.raw
@@ -32,15 +35,15 @@ export default function NotFoundContent({ className }) {
 888888888888 88         88 888888888888
         88   "8b       d8"         88  
         88    "8ba, ,ad8"          88  
-        88      "Y888P"            88  `
-              }
+        88      "Y888P"            88  `}
             </pre>
-            <p>
+            <p style={{ paddingTop: '1rem' }}>
               You've hit a broken URL. Please contact Jorban on Discord:{' '}
               <a
                 href="https://discord.gg/wY49KMxjHT"
                 target="_blank"
                 rel="noopener noreferrer"
+                style={{ color: '#7289da', wordBreak: 'break-word' }}
               >
                 https://discord.gg/wY49KMxjHT
               </a>
